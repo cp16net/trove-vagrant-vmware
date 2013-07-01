b629bdaa2252c7da9272e04ef95f35722f5f1138
Ed Cranford <ed.cranford@rackspace.com>
2013-07-01 17:35:18 -0500

    Updated install to mind /trove

--- install-trove.sh
+++ install-trove.sh
@@ -4,14 +4,18 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
 apt-get install -y --force-yes git-core
 
-# Get the code
-SHARE=/opt/stack
+# Code should be shared at /trove.
+# If it isn't, go get it.
+# Get the code. If /trove isn't populated, go get the pieces.
+mkdir -p /opt/stack
+SHARE=/trove
 mkdir -p $SHARE
 pushd $SHARE
     for REPO in python-troveclient trove trove-integration; do
         if [ ! -d "$SHARE/$REPO" ]; then
             git clone git://github.com/openstack/$REPO.git
         fi
+        ln -s $SHARE/$REPO /opt/stack/$REPO
     done
 popd
 
@@ -19,7 +23,8 @@ popd
 useradd trove -d /home/trove -s /bin/bash -m
 echo trove:trove | chpasswd
 sed -i '/^%sudo/a\trove ALL=(ALL) NOPASSWD:ALL' /etc/sudoers
-#sed -i '$a\cd /home/trove/trove-integration/scripts' /home/trove/.bashrc
+ln -s /opt/stack/trove-integration /home/trove/trove-integration
+sed -i '$a\cd /home/trove/trove-integration/scripts' /home/trove/.bashrc
 
 chown trove /opt/stack
 
