a662b41e5f263bc6c11fc65decb6c1ce176e43c5
Robert Myers <robert.myers@rackspace.com>
2013-12-10 10:39:00 -0600

    Reorganize the install scripts.
    * Adding an option for TROVE_VM_MEMORY
    * moving fix-iptables.sh to /usr/local/bin
    * adding redstack script to /usr/local/bin
    * adding a .my.cnf file

--- README.md
+++ README.md
@@ -2,23 +2,40 @@ You'll need VMWare Fusion, and the VMWare Fusion Provisioner for Vagrant.
 
 To get things started:
 
-    vagrant up --provider=vmware_fusion
-    vagrant ssh
-    ./redstack install
-    ./redstack kick-start mysql
-    ./fix-iptables.sh
-    ./redstack int-tests --group=blackbox
+    $ vagrant up --provider=vmware_fusion
+    $ vagrant ssh
 
-There are two optional environment variables the Vagrantfile looks for:
+Install Trove:
 
-    TROVE_VM_SOURCE_DIR
-        The directory on the host that will be shared with the VM.
-        If it's not specified, the directory above this directory (`./../') is used.
-&nbsp;
+    $ redstack install
+    $ redstack kick-start mysql
 
-    TROVE_VM_SYNC_DIR
-        The directory on the VM that the above directory will be available at.
-        If it's not specified, `/vagrant` is used.
+Running tests:
+
+    $ fix-iptables.sh
+    $ redstack int-tests --group=blackbox
+
+Using the local mysql client:
+
+    $ mysql
+    mysql> use trove
+    mysql> select * from instances;
+    Empty set (0.00 sec)
+
+There are three optional environment variables the Vagrantfile looks for:
+
+<dl>
+  <dt>TROVE_VM_SOURCE_DIR</dt>
+  <dd>The directory on the host that will be shared with the VM. If it's not
+      specified, the directory above this directory `./../` is used.</dd>
+
+  <dt>TROVE_VM_SYNC_DIR</dt>
+  <dd>The directory on the VM that the above directory will be available at.
+      If it's not specified, `/trove` is used.</dd>
+
+  <dt>TROVE_VM_MEMORY</dt>
+  <dd>Set the amount of RAM to use for the VM. Default is 2048</dd>
+</dl>
 
 
 Special thanks to Ryan Skoblenick:
--- install-trove.sh
+++ install-trove.sh
@@ -19,7 +19,7 @@ fi
 
 
 apt-get update -y -q
-apt-get install -y -q git-core libxml2-dev libxslt1-dev python-pexpect maven2 apache2 bc debhelper
+apt-get install -y -q git-core libxml2-dev libxslt1-dev python-pexpect apache2 bc debhelper curl
 
 SHARE=$1
 SHARE=${SHARE:-/trove}
@@ -27,16 +27,14 @@ echo Trove shared directory is $SHARE
 
 echo Linking Trove codebase.
 mkdir -p /opt/stack $SHARE
-pushd $SHARE
-    for REPO in python-troveclient trove trove-integration; do
-        if [ ! -d "$SHARE/$REPO" ]; then
-            git clone git://github.com/openstack/$REPO.git
-        fi
-        if [ ! -e "/opt/stack/$REPO" ]; then
-            ln -s $SHARE/$REPO /opt/stack/$REPO
-        fi
-    done
-popd
+for REPO in python-troveclient trove trove-integration; do
+    if [ ! -d "$SHARE/$REPO" ]; then
+        git clone git://github.com/openstack/$REPO.git $SHARE/$REPO
+    fi
+    if [ ! -e "/opt/stack/$REPO" ]; then
+        ln -s $SHARE/$REPO /opt/stack/$REPO
+    fi
+done
 
 
 echo Updating user vagrant.
@@ -45,7 +43,6 @@ if [ ! -e "$TROVE_INSTALLED" ]; then
     ln -s /opt/stack/trove-integration /home/vagrant/trove-integration
     sed -i '$a\export PATH=$PATH:/sbin' /home/vagrant/.bashrc
     sed -i '$a\export USE_UUID_TOKEN=True' /home/vagrant/.bashrc
-    sed -i '$a\cd /home/vagrant/trove-integration/scripts' /home/vagrant/.bashrc
     chown vagrant /opt/stack
     SSH_CFG=/home/vagrant/.ssh/config
     mkdir -p /home/vagrant/.ssh
@@ -56,28 +53,37 @@ if [ ! -e "$TROVE_INSTALLED" ]; then
 fi
 
 
-echo Installing Django manually.
-if [ ! -e "/usr/local/lib/python2.7/dist-packages/django" ]; then
-    pushd /tmp
-        wget http://pypi.python.org/packages/source/D/Django/Django-1.5.1.tar.gz#md5=7465f6383264ba167a9a031d6b058bff -O Django.tgz -q
-        tar xzf Django.tgz
-        pushd Django-1.5.1
-            python setup.py install > /dev/null 2>&1
-        popd
-        rm -rf Django*
-    popd
+echo Creating fix-iptables.sh
+FIXSH="/usr/local/bin/fix-iptables.sh"
+if [ ! -e "$FIXSH" ]; then
+    echo "#!/bin/bash" > $FIXSH
+    echo "sudo iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o eth0 -j MASQUERADE" >> $FIXSH
+    chmod +x $FIXSH
 fi
 
+echo Creating redstack command
+REDSTACK="/usr/local/bin/redstack"
+if [ ! -e "$REDSTACK" ]; then
+    echo "#!/bin/bash" > $REDSTACK
+    echo "pushd /opt/stack/trove-integration/scripts" >> $REDSTACK
+    echo "./redstack \$@" >> $REDSTACK
+    echo "popd" >> $REDSTACK
+    chmod +x $REDSTACK
+fi
 
-echo Creating fix-iptables.sh
-FIXSH="fix-iptables.sh"
-pushd /opt/stack/trove-integration/scripts
-    if [ ! -e "$FIXSH" ]; then
-            echo "#!/bin/bash" > $FIXSH
-            echo "sudo iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o eth0 -j MASQUERADE" >> $FIXSH
-            chmod +x $FIXSH
-    fi
-popd
+echo Adding .my.cnf file for easy DB access
+MYCNF="/home/vagrant/.my.cnf"
+if [ ! -e $MYCNF ]; then
+    # Set SERVICE_HOST to not require extra functions
+    SERVICE_HOST=localhost
+    source /opt/stack/trove-integration/scripts/redstack.rc
+    cat <<EOF >$MYCNF
+[client]
+user=root
+password=$MYSQL_PASSWORD
+EOF
+    chown vagrant $MYCNF
+fi
 
 
 echo Installed.
